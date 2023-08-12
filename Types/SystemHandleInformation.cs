cb19ed7609f12448c232e708deaa5f4174be21f5
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-08-12 15:35:17 +0200

    Update SystemHandleInformation.cs

--- Types/SystemHandleInformation.cs
+++ Types/SystemHandleInformation.cs
@@ -6,12 +6,8 @@ namespace Nefarius.Utilities.NtDll.Types;
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 [SuppressMessage("ReSharper", "InconsistentNaming")]
-public struct SYSTEM_HANDLE_INFORMATION
+internal struct SYSTEM_HANDLE_INFORMATION
 {
-    public int ProcessID;
-    public byte ObjectTypeNumber;
-    public byte Flags; // 0x01 = PROTECT_FROM_CLOSE, 0x02 = INHERIT
-    public ushort Handle;
-    public int Object_Pointer;
-    public UInt32 GrantedAccess;
+    public UInt32 HandleCount;
+    public IntPtr Handles;
 }
